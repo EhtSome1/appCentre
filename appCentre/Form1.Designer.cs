87e4362b9e20f765049d83517c3a355fa4deadcb
EhtSome1 <arppauu@gmail.com>
2022-12-31 15:24:07 +0200

    better binary to ip bage

--- appCentre/Form1.Designer.cs
+++ appCentre/Form1.Designer.cs
@@ -29,74 +29,52 @@ namespace appCentre
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BinaryTextBox = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.IPTextBox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.binaryToIPReturnButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(329, 229);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 49);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "To IP";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.4874F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.51261F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 326F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.85046F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.14953F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 375F));
+            this.tableLayoutPanel1.Controls.Add(this.IPTextBox, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BinaryTextBox, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.binaryToIPReturnButton, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 170F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 95F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 169F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 108F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(803, 451);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // BinaryTextBox
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(329, 170);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "To Binary";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox2.Location = new System.Drawing.Point(75, 185);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(175, 22);
-            this.textBox2.TabIndex = 3;
+            this.BinaryTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BinaryTextBox.Location = new System.Drawing.Point(3, 205);
+            this.BinaryTextBox.Multiline = true;
+            this.BinaryTextBox.Name = "BinaryTextBox";
+            this.BinaryTextBox.Size = new System.Drawing.Size(370, 107);
+            this.BinaryTextBox.TabIndex = 3;
+            this.BinaryTextBox.TextChanged += new System.EventHandler(this.BinaryTextBox_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(101, 56);
+            this.textBox1.Location = new System.Drawing.Point(126, 116);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -104,6 +82,39 @@ namespace appCentre
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Binary";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // IPTextBox
+            // 
+            this.IPTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.IPTextBox.Location = new System.Drawing.Point(430, 205);
+            this.IPTextBox.Multiline = true;
+            this.IPTextBox.Name = "IPTextBox";
+            this.IPTextBox.Size = new System.Drawing.Size(369, 107);
+            this.IPTextBox.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(553, 116);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(123, 55);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "IP";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // binaryToIPReturnButton
+            // 
+            this.binaryToIPReturnButton.Location = new System.Drawing.Point(3, 3);
+            this.binaryToIPReturnButton.Name = "binaryToIPReturnButton";
+            this.binaryToIPReturnButton.Size = new System.Drawing.Size(174, 65);
+            this.binaryToIPReturnButton.TabIndex = 6;
+            this.binaryToIPReturnButton.Text = "Return";
+            this.binaryToIPReturnButton.UseVisualStyleBackColor = true;
+            this.binaryToIPReturnButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -120,12 +131,12 @@ namespace appCentre
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox BinaryTextBox;
+        private System.Windows.Forms.TextBox IPTextBox;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button binaryToIPReturnButton;
     }
 }
 
--- appCentre/Form1.cs
+++ appCentre/Form1.cs
@@ -16,5 +16,15 @@ namespace appCentre
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BinaryTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
