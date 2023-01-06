2190da2bcd0b51e5b77cbf4d0a2ffd246b6443c6
Alpo Uusitalo <arppauu@gmail.com>
2023-01-06 15:05:13 +0200

    juu

--- appCentre/Form1.Designer.cs
+++ appCentre/Form1.Designer.cs
@@ -30,12 +30,17 @@ namespace appCentre
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.IPTextBox = new System.Windows.Forms.TextBox();
             this.BinaryTextBox = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.IPTextBox = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.binaryToIPReturnButton = new System.Windows.Forms.Button();
+            this.ToIPButton = new System.Windows.Forms.Button();
+            this.ToBinaryButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.toBinaryIPconvert = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -43,89 +48,144 @@ namespace appCentre
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.85046F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.14953F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 375F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 287F));
             this.tableLayoutPanel1.Controls.Add(this.IPTextBox, 2, 2);
             this.tableLayoutPanel1.Controls.Add(this.BinaryTextBox, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.textBox4, 2, 1);
             this.tableLayoutPanel1.Controls.Add(this.binaryToIPReturnButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ToIPButton, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.ToBinaryButton, 2, 3);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 95F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 169F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 108F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(803, 451);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 116F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 87F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 99F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(602, 366);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // IPTextBox
+            // 
+            this.IPTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.IPTextBox.Location = new System.Drawing.Point(319, 213);
+            this.IPTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.IPTextBox.Name = "IPTextBox";
+            this.IPTextBox.Size = new System.Drawing.Size(278, 20);
+            this.IPTextBox.TabIndex = 4;
             // 
             // BinaryTextBox
             // 
             this.BinaryTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.BinaryTextBox.Location = new System.Drawing.Point(3, 205);
-            this.BinaryTextBox.Multiline = true;
+            this.BinaryTextBox.Location = new System.Drawing.Point(2, 213);
+            this.BinaryTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.BinaryTextBox.Name = "BinaryTextBox";
-            this.BinaryTextBox.Size = new System.Drawing.Size(370, 107);
+            this.BinaryTextBox.Size = new System.Drawing.Size(272, 20);
             this.BinaryTextBox.TabIndex = 3;
-            this.BinaryTextBox.TextChanged += new System.EventHandler(this.BinaryTextBox_TextChanged);
             // 
             // textBox1
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(126, 116);
+            this.textBox1.Location = new System.Drawing.Point(92, 133);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(123, 55);
+            this.textBox1.Size = new System.Drawing.Size(92, 45);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Binary";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // IPTextBox
-            // 
-            this.IPTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.IPTextBox.Location = new System.Drawing.Point(430, 205);
-            this.IPTextBox.Multiline = true;
-            this.IPTextBox.Name = "IPTextBox";
-            this.IPTextBox.Size = new System.Drawing.Size(369, 107);
-            this.IPTextBox.TabIndex = 4;
             // 
             // textBox4
             // 
             this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(553, 116);
+            this.textBox4.Location = new System.Drawing.Point(412, 133);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(123, 55);
+            this.textBox4.Size = new System.Drawing.Size(92, 45);
             this.textBox4.TabIndex = 5;
             this.textBox4.Text = "IP";
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // binaryToIPReturnButton
             // 
-            this.binaryToIPReturnButton.Location = new System.Drawing.Point(3, 3);
+            this.binaryToIPReturnButton.Location = new System.Drawing.Point(2, 2);
+            this.binaryToIPReturnButton.Margin = new System.Windows.Forms.Padding(2);
             this.binaryToIPReturnButton.Name = "binaryToIPReturnButton";
-            this.binaryToIPReturnButton.Size = new System.Drawing.Size(174, 65);
+            this.binaryToIPReturnButton.Size = new System.Drawing.Size(130, 53);
             this.binaryToIPReturnButton.TabIndex = 6;
             this.binaryToIPReturnButton.Text = "Return";
             this.binaryToIPReturnButton.UseVisualStyleBackColor = true;
-            this.binaryToIPReturnButton.Click += new System.EventHandler(this.button1_Click);
+            this.binaryToIPReturnButton.Click += new System.EventHandler(this.binaryToIPReturnButton_Click);
+            // 
+            // ToIPButton
+            // 
+            this.ToIPButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ToIPButton.Location = new System.Drawing.Point(93, 270);
+            this.ToIPButton.Name = "ToIPButton";
+            this.ToIPButton.Size = new System.Drawing.Size(90, 31);
+            this.ToIPButton.TabIndex = 7;
+            this.ToIPButton.Text = "To IP";
+            this.ToIPButton.UseVisualStyleBackColor = true;
+            this.ToIPButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ToBinaryButton
+            // 
+            this.ToBinaryButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ToBinaryButton.Location = new System.Drawing.Point(410, 270);
+            this.ToBinaryButton.Name = "ToBinaryButton";
+            this.ToBinaryButton.Size = new System.Drawing.Size(96, 36);
+            this.ToBinaryButton.TabIndex = 8;
+            this.ToBinaryButton.Text = "To Binary";
+            this.ToBinaryButton.UseVisualStyleBackColor = true;
+            this.ToBinaryButton.Click += new System.EventHandler(this.ToBinaryButton_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.toBinaryIPconvert, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(602, 366);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // toBinaryIPconvert
+            // 
+            this.toBinaryIPconvert.Location = new System.Drawing.Point(3, 3);
+            this.toBinaryIPconvert.Name = "toBinaryIPconvert";
+            this.toBinaryIPconvert.Size = new System.Drawing.Size(87, 76);
+            this.toBinaryIPconvert.TabIndex = 0;
+            this.toBinaryIPconvert.Text = "binary-IP converter";
+            this.toBinaryIPconvert.UseVisualStyleBackColor = true;
+            this.toBinaryIPconvert.Click += new System.EventHandler(this.toBinaryIPconvert_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.ClientSize = new System.Drawing.Size(602, 366);
+            this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -137,6 +197,10 @@ namespace appCentre
         private System.Windows.Forms.TextBox IPTextBox;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button binaryToIPReturnButton;
+        private System.Windows.Forms.Button ToIPButton;
+        private System.Windows.Forms.Button ToBinaryButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button toBinaryIPconvert;
     }
 }
 
--- appCentre/Form1.cs
+++ appCentre/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 
 namespace appCentre
 {
+
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,14 +19,180 @@ namespace appCentre
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //binary to IP
+        {
+            string[] IPnumbers = new string[4];
+
+            string input = BinaryTextBox.Text;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 8 == 0)
+                    sb.Append(' ');
+                sb.Append(input[i]);
+            }
+            string formatted = sb.ToString();
+            string[] binaryNumbers = /*allBinaryNumbers*/formatted.Split(' ');
+
+            // translate
+            for(int m = 1; m < 5; m++)
+            {
+                int index = 0;
+                string number = binaryNumbers[m].ToString();
+                for (int o = 7; o >= 0; o--)
+                {
+                    if (number[o] == '1')
+                    {
+                        switch(o)
+                        {
+                            case 7:
+                                index += 1;
+                                break;
+                            case 6:
+                                index += 2;
+                                break;
+                            case 5:
+                                index += 4;
+                                break;
+                            case 4:
+                                index += 8;
+                                break;
+                            case 3:
+                                index += 16;
+                                break;
+                            case 2:
+                                index += 32;
+                                break;
+                            case 1:
+                                index += 64;
+                                break;
+                            case 0:
+                                index += 128;
+                                break;
+                        }
+                    }
+                }
+
+                IPnumbers[m-1] = index.ToString();
+            }
+            IPTextBox.Text = IPnumbers[0] + "." + IPnumbers[1] + "." + IPnumbers[2] + "." + IPnumbers[3];
+        }
+
+        private void ToBinaryButton_Click(object sender, EventArgs e) // IP to binary
+        {
+            string[] IP = IPTextBox.Text.Split('.');
+            string[] binary = new string[4];
+            List<int> numberlist = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int number = Convert.ToInt32(IP[i]);
+                if (number - 128 >= 0)
+                {
+                    number -= 128;
+                    numberlist.Add(1);
+                }
+                else if (number - 128 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                if (number - 64 >= 0)
+                {
+                    number -= 64;
+                    numberlist.Add(1);
+                }
+                else if (number - 64 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                if (number - 32 >= 0)
+                {
+                    number -= 32;
+                    numberlist.Add(1);
+                }
+                else if (number - 32 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                if (number - 16 >= 0)
+                {
+                    number -= 16;
+                    numberlist.Add(1);
+                }
+                else if (number - 16 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                if (number - 8 >= 0)
+                {
+                    number -= 8;
+                    numberlist.Add(1);
+                }
+                else if (number - 8 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                if (number - 4 >= 0)
+                {
+                    number -= 4;
+                    numberlist.Add(1);
+                }
+                else if (number - 4 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                if (number - 2 >= 0)
+                {
+                    number -= 2;
+                    numberlist.Add(1);
+                }
+                else if (number - 2 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                if (number - 1 >= 0)
+                {
+                    number -= 1;
+                    numberlist.Add(1);
+                }
+                else if (number - 1 < 0)
+                {
+                    numberlist.Add(0);
+                }
+
+                binary[i] = string.Join(null, numberlist);
+                numberlist.Clear();
+            }
+
+            BinaryTextBox.Text = binary[0] + binary[1] + binary[2] + binary[3];
+        }
+
+        private void binaryToIPReturnButton_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Enabled = false;
+            tableLayoutPanel1.Visible = false;
+            tableLayoutPanel2.Enabled = true;
+            tableLayoutPanel2.Visible = true;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
-        private void BinaryTextBox_TextChanged(object sender, EventArgs e)
+        private void toBinaryIPconvert_Click(object sender, EventArgs e)
         {
-
+            tableLayoutPanel2.Visible = false;
+            tableLayoutPanel2.Enabled = false;
+            tableLayoutPanel1.Enabled = true;
+            tableLayoutPanel1.Visible = true;
         }
     }
 }
